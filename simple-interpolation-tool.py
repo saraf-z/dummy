5820a41b9562722cf2858498a214e6f063d77611
saraf-z <sfdezamb@gmail.com>
2024-11-06 13:15:25 +0100

    Csv file and excell file reading and formating

--- simple-interpolation-tool.py
+++ simple-interpolation-tool.py
@@ -10,6 +10,31 @@
 #9. show data and graph
 
 #Imports
-#Upload function
+import pandas as pd
+
+#testing
+print('Script interpolator_script.py')
+
+#STEPS 1 AND 2 -------------------------------------------------------------------------------
+#Upload excel file
+file_path_excel =r"C:\Users\VPECOS\Desktop\documentos sara\PRACTICAS\datos.xlsx"
+df_excel = pd.read_excel(file_path_excel)#call to excel file
+print(df_excel.head)
+#Upload csv file
+file_path_csv = r'C:\Users\VPECOS\Desktop\documentos sara\PRACTICAS\datos.csv' ####REMEMBER TO UPLOAD CORRECT CSV FILE
+df = pd.read_csv(file_path_csv)
+print("CSV file loaded successfully.")
+print(df.head())
+# PRINT data in list format
+column1 = list(df['X'])
+column2 = list(df['Y'])
+#test the script
+energy = column1  # Energy values of the distribution
+values = column2  # Variable values of the distribution
+#print results
+print(f'Energy: {energy}')
+print (f'Values: {values}')
+# Data Read and listed --------------------------------------------------------------------------------------
+
 
 
