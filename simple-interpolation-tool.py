013370afd2b78d98c79caf72e3e17519c5cbe017
saraf-z <sfdezamb@gmail.com>
2024-11-06 11:15:29 +0100

    New beta interpolation-tool full function script

---
+++ simple-interpolation-tool.py
@@ -0,0 +1,15 @@
+#Python file created as a beta version for the interpolation tool
+#1. Upload csv- excell file
+#2. Read and format excell file
+#3. ask user for type of interpolation
+#4. ask user for interpolation data input
+#5. convert data to interpolate into logartithms
+#6. operate !
+#7. convert data to exp scale
+#8. create graph
+#9. show data and graph
+
+#Imports
+#Upload function
+
+
